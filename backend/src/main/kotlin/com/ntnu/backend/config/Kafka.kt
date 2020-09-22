ff195f056b4482ba531a7dfff52f4bb0f2b5b49f
Agnar22 <agnar.bjornstad@gmail.com>
2020-09-22 13:16:49 +0200

    Create topic on startup if it does not already exist.

--- backend/src/main/kotlin/com/ntnu/backend/config/Kafka.kt
+++ backend/src/main/kotlin/com/ntnu/backend/config/Kafka.kt
@@ -1,10 +1,13 @@
 package com.ntnu.backend.config
 
+import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaAdmin
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
 
@@ -26,6 +29,15 @@ class Kafka(properties: KafkaProperties) {
     )
 
 
+    @Bean
+    fun admin(): KafkaAdmin {
+        return KafkaAdmin(kafkaConfig)
+    }
+
+    @Bean
+    fun createTopic(): NewTopic {
+        return TopicBuilder.name(topic).build()
+    }
 
     @Bean
     fun producerFactory(): ProducerFactory<String, String> {
