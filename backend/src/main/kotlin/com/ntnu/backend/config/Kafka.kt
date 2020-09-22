4b75bd9477063237729a3771a72a7e77682c9c90
Agnar22 <agnar.bjornstad@gmail.com>
2020-09-22 13:13:39 +0200

    Connect to the kafka cluster hosted by NAV.

--- backend/src/main/kotlin/com/ntnu/backend/config/Kafka.kt
+++ backend/src/main/kotlin/com/ntnu/backend/config/Kafka.kt
@@ -1,34 +1,39 @@
 package com.ntnu.backend.config
 
-import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
 
 @Configuration
-class Kafka {
-    @Value("\${kafka.host:localhost}")
-    private val host: String? = null
+class Kafka(properties: KafkaProperties) {
 
-    @Value("\${kafka.port:9092}")
-    private val port: Int = 0
+    private val topic: String = properties.template.defaultTopic
+    private val hosts: MutableList<String> = properties.bootstrapServers
+    private val kafkaConfig : HashMap<String, Any> = hashMapOf(
+            "bootstrap.servers" to hosts,
+            "security.protocol" to "SSL",
+            "ssl.truststore.location" to "client.truststore.jks",
+            "ssl.truststore.password" to "truststore_password",
+            "ssl.keystore.type" to "PKCS12",
+            "ssl.keystore.location" to "client.keystore.p12",
+            "ssl.keystore.password" to "keystore_password",
+            "key.serializer" to StringSerializer::class.java,
+            "value.serializer" to StringSerializer::class.java
+    )
+
+
 
     @Bean
     fun producerFactory(): ProducerFactory<String, String> {
-        val configProps = HashMap<String, Any>()
-        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "$host:$port"
-        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        return DefaultKafkaProducerFactory(configProps)
+        return DefaultKafkaProducerFactory(kafkaConfig)
     }
 
     @Bean
     fun kafkaTemplate(): KafkaTemplate<String, String> {
         return KafkaTemplate(producerFactory())
     }
-
 }
