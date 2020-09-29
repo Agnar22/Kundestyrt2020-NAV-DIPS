c93a687fc3ab87199cfe65b00671039e53187ab3
Agnar22 <agnar.bjornstad@gmail.com>
2020-09-29 14:23:21 +0200

    Create basic test for endpoint.

---
+++ backend/src/test/kotlin/com/ntnu/backend/IntegrationTests.kt
@@ -0,0 +1,20 @@
+package com.ntnu.backend
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests {
+
+    @Autowired
+    lateinit var restTemplate: TestRestTemplate
+
+    @Test
+    fun `Assert that endpoint puts message on kafka`() {
+        val result = restTemplate.getForEntity("/testing", String::class.java)
+        Assertions.assertEquals("Published successfully", result.body);
+    }
+}
