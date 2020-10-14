9fb87753ce62cd9deaa3065edd5351019f74d2a0
Vebjørn <v.ohr97@gmail.com>
2020-10-14 09:23:42 +0200

    add read/write to timestamp file

--- backend/src/main/kotlin/com/ntnu/backend/service/FHIR.kt
+++ backend/src/main/kotlin/com/ntnu/backend/service/FHIR.kt
@@ -8,6 +8,10 @@ import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 import java.time.Instant
 import java.time.format.DateTimeFormatter
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 import kotlin.concurrent.thread
 
 
@@ -21,10 +25,18 @@ class FHIR(val kafkaTemplate: KafkaTemplate<String, String>) {
     val questionnaire : String = ""
 
 
+    fun writeTimestamp(lastUpdated: String){
+      Files.write(Paths.get("timestamps.txt"), (lastUpdated).toByteArray(), StandardOpenOption.WRITE)
+    }
+
+    fun readTimestamp(): String{
+      return String(Files.readAllBytes(Paths.get("timestamps.txt")))
+    }
+
     @Autowired
     fun listen_to_responses() {
         thread(start = true) {
-            var lastUpdated : String = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+            var lastUpdated : String = readTimestamp()
             val sleepTime : Long = 500
             while (true) {
                 val response = khttp.get("https://r3.smarthealthit.org/QuestionnaireResponse?questionnaire=${questionnaire}&authored=gt${lastUpdated}", headers = mapOf("Content-Type" to "application/fhir-json")).jsonObject
@@ -35,13 +47,18 @@ class FHIR(val kafkaTemplate: KafkaTemplate<String, String>) {
                     continue
                 }
                 val forms = response.getJSONArray("entry")
-                println("last updated ${lastUpdated}")
+
                 forms.forEach{
                     val form : JSONArray = (it as JSONObject).getJSONObject("resource").getJSONArray("item")
                     kafkaTemplate.send(topic, form.toString())
                 }
+
                 Thread.sleep(sleepTime)
+
                 lastUpdated = response.getJSONObject("meta").get("lastUpdated") as String
+                writeTimestamp(lastUpdated)
+
+
             }
         }
     }
