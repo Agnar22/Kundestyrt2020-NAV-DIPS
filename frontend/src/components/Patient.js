898db2be72e9169386c292253d13caf67ca569bb
Hanne Olsen <hannols@stud.ntnu.no>
2020-10-14 11:20:55 +0200

    Fixed lint errors

--- frontend/src/components/Patient.js
+++ frontend/src/components/Patient.js
@@ -11,7 +11,7 @@ import MomentUtils from '@date-io/moment';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import FhirClientContext from '../FhirClientContext';
 import QuestionnaireResponseTemplate from '../QuestionnaireResponseTemplate.json';
-//import { responsiveFontSizes } from '@material-ui/core';
+// import { responsiveFontSizes } from '@material-ui/core';
 
 moment.locale('nb'); // Set calendar to be norwegian (bokmaal)
 
@@ -56,30 +56,6 @@ export default class Patient extends React.Component {
       };
     }
 
-    //Gets QuestionnaireResponseTemplate.json and fills out with current patients information, before returning the form. 
-    getAndFillResponseForm = (status) => {
-        const fullPatientName = `${this.state.patient.name[0].given.join(" ")} ${this.state.patient.name[0].family}`;
-        const socialSecurityNumber = this.state.patient.identifier.find((sb) => sb.system === 'http://hl7.org/fhir/sid/us-ssn').value;
-
-        const responseForm=QuestionnaireResponseTemplate;
-        responseForm.subject.reference = `Patient/${this.state.patient.id}`;
-        responseForm.subject.display = fullPatientName;
-        responseForm.item[0].answer[0].valueString = fullPatientName;
-        responseForm.item[1].answer[0].valueString = socialSecurityNumber;
-        responseForm.item[2].answer[0].valueString = this.state.startDate._d;
-        responseForm.item[3].answer[0].valueString = this.state.endDate._d;
-        responseForm.item[4].answer[0].valueString = this.state.value;
-
-        //Gets the fhirUser-ID of the practitioner and fills it in the form
-        responseForm.author.reference= this.context.client.user.fhirUser;
-
-        //Sets the status of the QuestionnaireResponse-form to the functions argument 
-        responseForm.status=status;
-         
-
-        return responseForm;
-    } 
-
     async componentDidMount() {
       const { client } = this.context;
       this._loader = await client.patient
@@ -92,39 +68,64 @@ export default class Patient extends React.Component {
         });
     }
 
+    // Gets QuestionnaireResponseTemplate.json and fills out with current patients information,
+    // before returning the form.
+    getAndFillResponseForm = (status) => {
+      const fullPatientName = `${this.state.patient.name[0].given
+        .join(' ')} ${this.state.patient.name[0].family}`;
+      const socialSecurityNumber = this.state.patient.identifier
+        .find((sb) => sb.system === 'http://hl7.org/fhir/sid/us-ssn').value;
+
+      const responseForm = QuestionnaireResponseTemplate;
+      responseForm.subject.reference = `Patient/${this.state.patient.id}`;
+      responseForm.subject.display = fullPatientName;
+      responseForm.item[0].answer[0].valueString = fullPatientName;
+      responseForm.item[1].answer[0].valueString = socialSecurityNumber;
+      responseForm.item[2].answer[0].valueString = this.state.startDate._d;
+      responseForm.item[3].answer[0].valueString = this.state.endDate._d;
+      responseForm.item[4].answer[0].valueString = this.state.value;
+
+      // Gets the fhirUser-ID of the practitioner and fills it in the form
+      responseForm.author.reference = this.context.client.user.fhirUser;
+
+      // Sets the status of the QuestionnaireResponse-form to the functions argument
+      responseForm.status = status;
+
+      return responseForm;
+    }
 
     handleChange = (event) => {
       this.setState({ value: event.target.value });
     }
 
-    //Function for saving a questionnaire response for current patient
-    handleSave = (event) =>{
-        //ToDo: Make more functionality for saving form 
-        const filledResponse = this.getAndFillResponseForm("in-progress");
-        console.log(filledResponse);
+    // Function for saving a questionnaire response for current patient
+    handleSave = (event) => {
+      event.preventDefault();
+      // ToDo: Make more functionality for saving form
+      const filledResponse = this.getAndFillResponseForm('in-progress');
+      console.log(filledResponse);
     }
 
-    //Function for sending an questionnaire response for current patient
+    // Function for sending an questionnaire response for current patient
     handleSubmit = (event) => {
       event.preventDefault();
 
       const fhirclient = this.context.client;
-      
+
       const headers = {
         'Content-Type': 'application/fhir+json',
         Accept: '*/*',
       };
 
       const options = {
-        url: `https://r3.smarthealthit.org/QuestionnaireResponse`,
-        body: JSON.stringify(this.getAndFillResponseForm("completed")),
+        url: 'https://r3.smarthealthit.org/QuestionnaireResponse',
+        body: JSON.stringify(this.getAndFillResponseForm('completed')),
         headers,
         method: 'POST',
       };
 
       fhirclient.request(options);
     }
-
 
     /* eslint-disable react/jsx-props-no-spreading */
     render() {
